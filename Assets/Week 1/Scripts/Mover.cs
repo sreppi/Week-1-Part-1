30570731e83921ab63faca65ff8571e30e20f346
XPC\calvi <calvinip@live.ca>
2024-01-10 15:17:21 -0500

    Task 2-b Complete, added tanks, mover, and missile spawner.

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     float speed = 3f;
+    public GameObject missilePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,12 @@ public class Mover : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab, spawn.position, transform.rotation);
+
+        }
+
     }
 }
